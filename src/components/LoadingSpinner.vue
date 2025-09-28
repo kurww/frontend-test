161f40e931b671e71e778d1156a958a617a4813d
www <56748816+kurww@users.noreply.github.com>
2025-09-28 21:07:35 +0700

    Update LoadingSpinner.vue

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -8,7 +8,7 @@
     <p class="loading-text">Congratulations, You Did It!</p>
     <p class="loading-text">playing now</p>
     <iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="175" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/id/song/menghancurkan/1760139686"></iframe>
-    <p class="loading-subtext">Your changes have been successfully deployed. Ade Kurniawan. v3</p>
+    <p class="loading-subtext">Your changes have been successfully deployed. Ade Kurniawan. v4</p>
   </div>
 </template>
 
